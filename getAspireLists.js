b142cae6350dd8d918643e0b3c270773889a968e
alfi1 <t.c.graves@sussex.ac.uk>
2017-01-18 13:32:38 +0000

    Nearly complete
    Just needs ng-show to hide it when no lists are retrieved

--- getAspireLists.js
+++ getAspireLists.js
@@ -1,19 +1,18 @@
+//THIS IS BLOODY IT!!
 // 17/01/2017 - another breakthrough. This version allows me to get the MMSID, and check Aspire using $http
 // Version saved to github
 // Now lists the Aspire lists on the display
+// 18/01/2017. Another breakthrough - displays the results using ng-repeat!
 
   var app = angular.module('viewCustom', ['angularLoad']);
 
-   app.component('prmServiceDetailsAfter', {
+       app.component('prmServiceDetailsAfter', {
 	   bindings: { parentCtrl: '<' },
 	   controller: 'DisplayAspireListsController',
-	   //template: '<div> <span class="bold text">Aspire lists </span>{{$ctrl.getResults()}}</div>'
-	   //template: '<div> <span class="bold text">Cited on reading lists:</span><br>{{$ctrl.getResults()}}</div>'
-	   
-	   //template: '<ul>  <li ng-repeat="x in {{$ctrl.getResults()}}" > {{ x }} </li></ul>'
-
-	   
-	   template: '<div> <span class="bold text">Cited on reading lists:</span><br>{{$ctrl.getResults()}}</div>'
+	   	   
+	  //template: '<div><span class="bold text">Cited on reading lists:</span><br><ul><li ng-repeat="category in listsFound">Line {{ category }}</li></ul></div>'
+	   template: '<div><span class="bold text">Cited on reading lists:</span><ul><li ng-repeat="(url,listname) in listsFound"><a href="{{url}}">{{listname}} </a></li></ul></div>'
+	
 	   
      });
    
@@ -21,9 +20,6 @@
 	
 	var vm = this;
 
-   // Set up an empty array ready for the Aspire lists
-   var results = [];
-   
 	// Get the MMSID
     vm.getMMSID = getMMSID;
  
@@ -34,25 +30,12 @@
    	// Make the call to Aspire
    var url = 'http://liblists.sussex.ac.uk/lcn/' + getMMSID() + '/lists.json?cb=JSON_CALLBACK';
    $http.jsonp(url)
-       
+   
           .success(function(response) {
-           for (var uri in response) {
-			 // Push them into an array
-			 results.push(uri + response[uri]);
-			 			 
-           };
-	
-   	     vm.getResults = getResults;
-		
-// Trying to get a way to pass teh scope to the directive template		
-		function getResults($scope) {
-		//function getResults() {
-         // test value to work out how to get the arrays iterating in the directive template
-		 //results = 'Hello World';
-		return results;
-
-		}
-		  
-      })
+			  	$scope.listsFound = response;	
+				// alert('http call succeeds');		
+		     //$scope.listsFound = results;				 
+ 
+      });
 
 });
