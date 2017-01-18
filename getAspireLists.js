cdeac49df2103d543c7fe0380a3b3a27e2e650ba
alfi1 <t.c.graves@sussex.ac.uk>
2017-01-18 13:50:01 +0000

    Final version. 18/01/2017
    Gets MMSID
    Calls Talis Aspire API
    Displays results for items found on any lists

--- getAspireLists.js
+++ getAspireLists.js
@@ -1,20 +1,13 @@
 //THIS IS BLOODY IT!!
-// 17/01/2017 - another breakthrough. This version allows me to get the MMSID, and check Aspire using $http
-// Version saved to github
-// Now lists the Aspire lists on the display
-// 18/01/2017. Another breakthrough - displays the results using ng-repeat!
+// 18/01/2016
 
   var app = angular.module('viewCustom', ['angularLoad']);
 
        app.component('prmServiceDetailsAfter', {
 	   bindings: { parentCtrl: '<' },
 	   controller: 'DisplayAspireListsController',
-	   	   
-	  //template: '<div><span class="bold text">Cited on reading lists:</span><br><ul><li ng-repeat="category in listsFound">Line {{ category }}</li></ul></div>'
-	   template: '<div><span class="bold text">Cited on reading lists:</span><ul><li ng-repeat="(url,listname) in listsFound"><a href="{{url}}">{{listname}} </a></li></ul></div>'
-	
-	   
-     });
+	   template: '<div ng-show="listsFound != null"><span class="bold text">Cited on reading lists:</span><ul><li ng-repeat="(url,listname) in listsFound"><a href="{{url}}">{{listname}} </a></li></ul></div>'
+	   });
    
 	app.controller('DisplayAspireListsController', function ($scope, $http) {
 	
@@ -33,9 +26,6 @@
    
           .success(function(response) {
 			  	$scope.listsFound = response;	
-				// alert('http call succeeds');		
-		     //$scope.listsFound = results;				 
- 
       });
 
 });
